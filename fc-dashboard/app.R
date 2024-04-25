e5c012a698e36395cfb91557fb8ece03523effa6
Siobhan Bradshaw <siobhan.bradshaw@churcharmy.org>
2024-04-25 15:55:30 +0100

    committing the new queries csvs

--- fc-dashboard/app.R
+++ fc-dashboard/app.R
@@ -2,13 +2,60 @@ library(shiny)
 library(scales)
 library(palmerpenguins)
 library(carutools)
-library(ggplot2)
+library(vroom)
+library(lubridate)
+library(janitor)
+library(tidyverse)
 
 placeholder_plot <-
   ggplot(penguins, aes(x = bill_depth_mm, y = bill_length_mm,
                        colour = species)) +
   geom_point() +
   theme_ca("black")
+
+#-------------------------------------------------------------------------------
+
+## Helpers --------------------------------------------------------------------
+monthify <- function(x){
+  month <- month(x)
+  year <- year(x)
+  make_date(year, month)
+}
+
+weekify <- function(x){
+  week <- week(x)
+  year <- year(x)
+
+  weeks_to_add <- map_vec(week, \(x) period(x, "weeks"))
+
+  make_date(year) + weeks_to_add
+}
+
+
+## Read in Data ----------------------------------------------------------------
+
+### Mailchimp data -------------------------------------------------------------
+mailchimp <-
+  vroom("app-inputs/mailchimp.csv") |>
+  clean_names() |>
+  mutate(month = monthify(email_sent_time),
+         week = weekify(email_sent_time))
+mailchimp <-
+  mutate(
+    mailchimp,
+
+    email_type =
+      case_when(
+        str_detect(email_name, "CAConnected") ~ "Connected",
+        str_detect(email_name, "Prayer Points") ~ "Prayer",
+        str_detect(email_name, "Inside Out") ~ "Inside Out",
+        str_detect(email_name, "Appeal") ~ "Appeals",
+        str_detect(email_name, "Supporter News") ~ "Supporter news",
+        .default = "Other") |>
+      ordered() |>
+      fct_relevel("Other", after = Inf)
+  )
+#-------------------------------------------------------------------------------
 
 ## TODO:
 ## * Create function that applies default theme elements to each plot
@@ -56,8 +103,8 @@ ui <- fluidPage(
              ),
 
     tabPanel("Online engagement",
-
-             plotOutput("online_engagement_plot")
+             plotOutput("online_engagement_plot"),
+             plotOutput("mailchimp_weekday_plot")
              )
 ))
 
@@ -96,8 +143,66 @@ server <- function(input, output){
   #### Online engagement -------------------------------------------------------
 
   output$online_engagement_plot <- renderPlot({
-    placeholder_plot +
+
+    summarised <-
+      mailchimp |>
+      group_by(month, email_type) |>
+      summarise(mean_click_rate = mean(click_rate, na.rm = TRUE))
+
+    ggplot(summarised, aes(x = month, y = mean_click_rate)) +
+
+      geom_hline(yintercept = 0.021,
+                 colour = ca_maroon(),
+                 linewidth = 1.3,
+                 linetype = "dashed") +
+
+      geom_line(colour = ca_cyan(),
+                linewidth = 1.5) +
+
+      facet_wrap(~ email_type) +
+
+      ggtitle("Click Rate Over Time") +
+
+      xlab("Time") +
+
+      scale_y_continuous(name = "Click rate", labels = percent_format()) +
+
+      theme_minimal() +
+
+      theme(text = element_text(family = "Trebuchet MS")) +
+
       ggtitle("Online Engagement")
+
+  })
+
+
+
+  output$mailchimp_weekday_plot <- renderPlot({
+
+
+    mailchimp |>
+
+      mutate(weekday  = wday(email_sent_time, label = TRUE)) |>
+
+      mutate(weekday_click_rate = mean(click_rate), .by = weekday) |>
+
+      summarise(click_rate = mean(click_rate), .by = c(email_type, weekday, weekday_click_rate)) |>
+
+           ggplot(
+                 aes(y = click_rate,
+                     x = weekday,
+                     fill = email_type,
+                     colour = email_type
+                     )) +
+
+           geom_point(size = 6, alpha = 0.75, shape = 21, stroke = 1.5) +
+
+      ca_scale_fill_discrete() +
+      ca_scale_colour_discrete() +
+
+      scale_y_continuous(labels = percent_format()) +
+
+      theme_minimal()
 
   })
 }
