a60eba837ce7a152b6d47f2a8b293b5bd5c142a5
Siobhan Bradshaw <siobhan.bradshaw@churcharmy.org>
2024-05-07 10:47:54 +0100

    important improvements to graphs and themes

--- fc-dashboard/app.R
+++ fc-dashboard/app.R
@@ -33,7 +33,15 @@ weekify <- function(x){
   make_date(year) + weeks_to_add
 }
 
-decrypt_data <- function(path, key_path = "app-secrets/rsa-key.RDS"){
+
+theme_set(
+  theme_minimal(base_size = 18) +
+    theme(text = element_text(family = "Trebuchet MS"))
+  )
+
+
+
+decrypt_data <- function(path, key_path = "app-secrets/rsa-key.RDS", ...){
 
   envelope <- readRDS(path)
   key <- readRDS(key_path)
@@ -43,16 +51,26 @@ decrypt_data <- function(path, key_path = "app-secrets/rsa-key.RDS"){
   data |>
     rawToChar() |>
     I() |>
-    vroom()
+    vroom(...)
 }
 
 ## Read in Data ----------------------------------------------------------------
 
 ### Raiser's edge data ---------------------------------------------------------
 
-query_1 <- decrypt_data("app-inputs/raisers-edge-query_1_encrypted-csv.RDS")
+query_1 <-
+  decrypt_data("app-inputs/raisers-edge-query_1_encrypted-csv.RDS",
+               col_types = "dcficccff") |>
+  mutate(gift_date = dmy(gift_date))
 
-query_2 <- decrypt_data("app-inputs/raisers-edge-query_2_encrypted-csv.RDS")
+query_2 <- decrypt_data("app-inputs/raisers-edge-query_2_encrypted-csv.RDS",
+                        col_types = "dcficccff")
+
+
+donor_id <- str_c("d_", 1:(round(nrow(query_1)/3)))
+query_1$donor_id <- sample(donor_id, nrow(query_1), replace = TRUE)
+
+
 
 ### Mailchimp data -------------------------------------------------------------
 mailchimp <-
@@ -78,24 +96,32 @@ mailchimp <-
       fct_relevel("Other", after = Inf)
   )
 #-------------------------------------------------------------------------------
-
-## TODO:
-## * Create function that applies default theme elements to each plot
-
-# Define UI for application that draws a histogram
 ui <- fluidPage(
 
-  tabsetPanel(
+tabsetPanel(
 
     tabPanel("Homepage",
 
-               p("We have recived", textOutput("received_this_month", inline = TRUE),
-               " so far this month",
-               " from ", textOutput("donors_this_month", inline = TRUE), " donors.",
-               style = "font-size: 25px;"),
+             div(style = c("font-size:30px; text-align:center; font-family:Trebuchet MS"),
+
+               p("In March, we recieved"),
+
+               p(textOutput("received_this_month", inline = TRUE),
+                 div(" from", style = "color:black"),
+                 style = "color:orange"),
+
+               p(textOutput("donors_this_month", inline = TRUE),
+                  style = "color:orange"),
+
+               p(" donors.")),
+
+
 
              plotOutput("giving_overview_plot")
              ),
+
+
+
 
     tabPanel("Individual donor stats",
 
@@ -103,14 +129,24 @@ ui <- fluidPage(
                sidebarPanel(
 
                  checkboxGroupInput("donation_form", "Form of donation",
-                             choices = c("Cash", "Cheque", "BACS")),
-                 selectInput("donation_reservation", "Reserved for:",
-                             choices = c("All", "Unreserved", "Waterways", "Amber",
-                                         "Marylebone")
+                             choices = c("Standing Order", "Direct Debit", "Personal Cheque",
+                                         "Voucher", "Cash", "Credit Card", "Business Cheque",
+                                         "Other", "PayPal"),
+                             selected = c("Standing Order", "Direct Debit", "Personal Cheque",
+                                          "Voucher", "Cash", "Credit Card", "Business Cheque",
+                                          "Other", "PayPal")),
+
+                 checkboxGroupInput("donation_reservation", "Reserved for:",
+                             choices = c("General unrestricted", "Centres of Mission",
+                                         "Marylebone", "Amber", "Ruby", "Personal support",
+                                         "MYCN"),
+                             selected = c("General unrestricted", "Centres of Mission",
+                                         "Marylebone", "Amber", "Ruby", "Personal support",
+                                         "MYCN")
                              ),
                  dateRangeInput("individual_donation_dates",
                            "Donate between",
-                           start = Sys.Date() - 30,
+                           start = Sys.Date() - 60,
                            end = Sys.Date())
                ),
                mainPanel(
@@ -127,23 +163,84 @@ ui <- fluidPage(
     tabPanel("Online engagement",
              plotOutput("online_engagement_plot"),
              plotOutput("mailchimp_weekday_plot")
-             )
-))
+    )))
 
 
 server <- function(input, output){
 
-  #### Homepage ----------------------------------------------------------------
-  output$received_this_month <- renderText(label_dollar(prefix = "£")(sum(query_1$`Gift Amount`)))
 
-  output$donors_this_month   <- renderText(label_comma()(100))
+
+max_date <- max(query_1$gift_date)
+
+max_month <- month(max_date)
+
+max_year <- year(max_date)
+
+month_start <- make_date(max_year, max_month)
+
+month_end <- month_start + period(1, "months") - period(1, "days")
+
+
+  #### Homepage ----------------------------------------------------------------
+  output$received_this_month <- renderText({
+
+  month_in_question <- filter(query_1, gift_date >= month_start, gift_date <= month_end)
+
+     label_dollar(prefix = "£")(sum(month_in_question$gift_amount_gbp))
+
+     })
+
+  output$month_label <- renderText(month.name[max_month])
+
+
+
+
+  output$donors_this_month <- renderText({
+
+    month_in_question <- filter(query_1, gift_date >= month_start, gift_date <= month_end)
+
+    unique_donors <- n_distinct(month_in_question$donor_id)
+
+    label_comma()(unique_donors)
+
+    })
 
 
   #### Individual donors -------------------------------------------------------
 
   output$forms_times_donation_plot <- renderPlot({
-    placeholder_plot +
-      ggtitle("Individual donations")
+    processed <-
+      query_1 |>
+      mutate(new_fund_description =
+               case_when(
+                 fund_description == "General unrestricted"   ~ "General unrestricted",
+                 str_detect(fund_description, "^COM ")        ~ "Centres of Mission",
+                 str_detect(fund_description, "^Marylebone ") ~ "Marylebone",
+                 str_detect(fund_description, "^Amber ")      ~ "Amber",
+                 str_detect(fund_description, "^Ruby ")       ~ "Ruby",
+                 str_detect(fund_description, "^Pers Supp ")  ~ "Personal support",
+                 str_detect(fund_description, "^MYCN ")       ~ "MYCN"
+                 ) |>
+               factor()
+             ) |>
+      filter(
+        gift_date > input$individual_donation_dates[1],
+        gift_date < input$individual_donation_dates[2],
+        gift_payment_type %in% input$donation_form,
+        new_fund_description %in% input$donation_reservation
+      ) |>
+
+      arrange(gift_date) |>
+      mutate(cumulative_gift_gbp = cumsum(gift_amount_gbp))
+
+      ggplot(processed,
+             aes(x = gift_date, y = cumulative_gift_gbp)) +
+        geom_line(colour = ca_purple(),
+                  linewidth = 1.5) +
+      labs(x = "Gift Date",
+           y = "Cumulative gift amounts",
+           title = "Cumulative giving from individual donors") +
+        theme(text = element_text(family = "Trebuchet MS"))
 
   })
 
@@ -181,11 +278,7 @@ server <- function(input, output){
 
       scale_y_continuous(name = "Click rate", labels = percent_format()) +
 
-      theme_minimal() +
-
-      theme(text = element_text(family = "Trebuchet MS")) +
-
-      ggtitle("Online Engagement")
+      ggtitle("Email click rates")
 
   })
 
@@ -214,19 +307,14 @@ server <- function(input, output){
                  colour = email_type
                  )) +
 
-      geom_point(aes(size = mean_emails),
-                 alpha = 0.75, shape = 21, stroke = 1.5) +
-
-      geom_line(data = average_click_rate, aes(group = 1),
-                linetype = "dashed", linewidth = 1, alpha = 0.8) +
+      geom_point(
+                 alpha = 0.75, shape = 21, stroke = 1.5,
+                 size = 5) +
 
       ca_scale_fill_discrete(name = "Email type") +
       ca_scale_colour_discrete(name = "Email type") +
-      scale_size_area(name = "Average emails", max_size = 9) +
 
       scale_y_continuous(labels = percent_format()) +
-
-      theme_minimal() +
 
       labs(
         x = "Weekday",
